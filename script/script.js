4ffe4437c903baf8c13ad54d449c834ac1db39c1
Saeedafshari11235 <msafshari11235@gmail.com>
2023-03-17 14:04:38 +0330

    adding event listener for colors

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
 let input = document.querySelector("input")
+let spans = document.querySelectorAll("span")
 input.addEventListener("keydown",(event)=>{
     if(event.key === "Enter"){
         event.preventDefault()
         console.log(input.value)
         input.value=""
     }
+})
+spans.forEach((span)=>{
+    span.addEventListener("click",()=>{
+        console.log(span.classList.value)
+        input.classList = []
+        input.classList.add(span.classList.value)
+    })
 })

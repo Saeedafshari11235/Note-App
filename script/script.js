57254f89270d47c211678340cbc00beb5c856b25
Saeedafshari11235 <msafshari11235@gmail.com>
2023-03-19 17:13:38 +0330

    adding modal for removing all notes

--- script/script.js
+++ script/script.js
@@ -3,6 +3,10 @@ let spans = document.querySelectorAll("span")
 let container = document.querySelector(".container")
 let plus = document.querySelector(".plus")
 let erase = document.querySelector(".eraser")
+let blur = document.querySelector("div.blur")
+let modal = document.querySelector(".modal")
+let yes = document.querySelector(".yes")
+let no = document.querySelector(".no")
 
 input.addEventListener("keydown",(event)=>{
     if(event.key === "Enter"){
@@ -14,10 +18,27 @@ input.addEventListener("keydown",(event)=>{
 plus.addEventListener("click",adder)
 
 erase.addEventListener("click",()=>{
+    blur.style.visibility = "visible"
+    blur.style.zIndex = "50"
+    modal.style.top = "calc(50vh - 3.5rem)"
+})
+
+yes.addEventListener("click",()=>{
     while(container.firstChild){
         container.removeChild(container.firstChild)
     }
+    modalremove()
 })
+
+no.addEventListener("click",()=>{
+    modalremove()
+})
+
+function modalremove(){
+    modal.style.top = "-30rem"
+    blur.style.visibility = "hidden"
+    blur.style.zIndex = "-1"
+}
 
 spans.forEach((span)=>{
     span.addEventListener("click",()=>{
